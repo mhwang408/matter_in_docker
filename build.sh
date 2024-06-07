463acbae6d5b345377ffb26706f92041e9139678
mh_wang <mhwang408@gmail.com>
2024-06-07 17:37:11 +0800

    removed --allow-changing-global-git-config because this can fail

--- build.sh
+++ build.sh
@@ -9,8 +9,10 @@ if [ -z "$@" ]; then
 	exit 1
 fi
 
+# [ ! -d $PROJECT_ROOT ] && git clone https://github.com/project-chip/connectedhomeip.git && cd ${PROJECT_ROOT} \
+# 	&& ./scripts/checkout_submodules.py --allow-changing-global-git-config --shallow --platform ${TARGET} && source scripts/bootstrap.sh
 [ ! -d $PROJECT_ROOT ] && git clone https://github.com/project-chip/connectedhomeip.git && cd ${PROJECT_ROOT} \
-	&& ./scripts/checkout_submodules.py --allow-changing-global-git-config --shallow --platform ${TARGET} && source scripts/bootstrap.sh
+	&& ./scripts/checkout_submodules.py --shallow --platform ${TARGET} && source scripts/bootstrap.sh
 # git pull
 # git submodule update --init
 cd ${PROJECT_ROOT} 
